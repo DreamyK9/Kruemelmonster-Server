7d5bea02c52bd9362b57b466e4dad7d0ec259b54
DreamDev621 <it.wolfx64@gmail.com>
2023-03-01 17:36:47 +0100

    created server

---
+++ server.js
@@ -0,0 +1,20 @@
+// Import the HTTP library
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    // runs when a request is received
+    console.log(request.url);
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+    // runs when the server is started
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+ 
